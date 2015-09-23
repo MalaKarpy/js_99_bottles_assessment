d2fb80ffe6d985a5192ad92914fd30e5eb06ddff
MalaKarpy <karpymala@gmail.com>
2015-09-22 23:20:53 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,5 @@
 var numberOfBottlesSong = function(numberBottles) {
 
-    //debugger;
-
     var i = numberBottles;
     var totalNumber_bottles = numberBottles;
     var partOfSentence1 = " bottles of beer on the wall, ";
@@ -11,26 +9,26 @@ var numberOfBottlesSong = function(numberBottles) {
     var oneLessBottle = i - 1;
     var outputSong = 0;
 
-     outputSong = i + partOfSentence1 + i + partOfSEntence2  +  oneLessBottle + partOfSEntence3;
+    outputSong = i + partOfSentence1 + i + partOfSEntence2  +  oneLessBottle + partOfSEntence3;
 
-     if(i === 0){
+    if(i === 0){
       var partOfSentence4 = " No more";
       var partOfSentence5 = " bottles of beer.";
       var partOfSentence6 = " Go to the store and buy some more,"
       //var zeroLyrics = 0;
       outputSong = partOfSentence4 + partOfSentence1 + partOfSentence4 + partOfSentence5 + partOfSentence6 + totalNumber_bottles + partOfSEntence3;
-     }
+    }
 
      return outputSong;
 };
 
 $(function() {
-     $("form#bottles-song").submit(function(event) {
+    $("form#bottles-song").submit(function(event) {
        var numberBottles = $("input#totalBottles").val()
 
        for(var i=numberBottles; i>=0; i--){
           $(".answer").append(numberOfBottlesSong(i) + "<br/>");
        }
        event.preventDefault();
-     });
+    });
 });
